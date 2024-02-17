e5cabcd7d97b5617273f5308212d48cdafa2c289
Daly <dalyna64@gmail.com>
2024-02-18 00:53:14 +0100

    issues table update

--- app/(pages)/issues/page.tsx
+++ app/(pages)/issues/page.tsx
@@ -1,13 +1,14 @@
 "use client";
 import { getAllIssues } from "@/utils/client";
 import { Issue } from "@prisma/client";
-import { Button, Table } from "@radix-ui/themes";
+import { Badge, Button, Table, TableCell } from "@radix-ui/themes";
 import Link from "next/link";
 import React from "react";
 import { useQuery } from "react-query";
 
-import { format, formatDistance, formatRelative, subDays } from "date-fns";
-import { enUS, fr } from "date-fns/locale";
+import { format, formatDistance } from "date-fns";
+import { MdDeleteForever } from "react-icons/md";
+
 const page = () => {
   const { data, isLoading, error } = useQuery("issues", getAllIssues);
 
@@ -32,7 +33,8 @@ const page = () => {
             <Table.ColumnHeaderCell>Description</Table.ColumnHeaderCell>
             <Table.ColumnHeaderCell>Status</Table.ColumnHeaderCell>
             <Table.ColumnHeaderCell>Created At</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell>updated At</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell>Last update</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell>Action</Table.ColumnHeaderCell>
           </Table.Row>
         </Table.Header>
         <Table.Body>
@@ -40,7 +42,9 @@ const page = () => {
             <Table.Row key={issue.id}>
               <Table.RowHeaderCell>{issue.title}</Table.RowHeaderCell>
               <Table.Cell>{issue.description}</Table.Cell>
-              <Table.Cell>{issue.status}</Table.Cell>
+              <Table.Cell>
+                <Badge color="green">{issue.status}</Badge>
+              </Table.Cell>
               <Table.Cell>
                 {format(issue.createdAt, "dd/LL/yyyy - hh:mm")}
               </Table.Cell>
@@ -48,6 +52,12 @@ const page = () => {
                 {formatDistance(issue.updatedAt, Date.now(), {
                   addSuffix: true,
                 })}
+              </Table.Cell>
+              <Table.Cell>
+                <Button color="red" variant="soft">
+                  <MdDeleteForever size="20" />
+                  delete
+                </Button>
               </Table.Cell>
             </Table.Row>
           ))}
