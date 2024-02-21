d58b0570e1b7fa82fd23148a213e313d162b2f63
Daly <dalyna64@gmail.com>
2024-02-21 20:19:45 +0100

    update issues query

--- app/(pages)/issues/page.tsx
+++ app/(pages)/issues/page.tsx
@@ -2,24 +2,20 @@
 import Error from "@/components/Error";
 import IssuesTable from "@/components/IssuesTable";
 import Loading from "@/components/Loading";
-import { getAllIssues } from "@/utils/client";
-import { Issue } from "@prisma/client";
+import { issueQuery } from "@/utils/client";
 import { Button } from "@radix-ui/themes";
-
 import { useRouter } from "next/navigation";
 import { MdAdd } from "react-icons/md";
-import { useQuery } from "react-query";
 
 const page = () => {
-  const { data, isLoading, error } = useQuery("issues", getAllIssues);
   const router = useRouter();
-  const issues = data?.data as Issue[];
   const goToNewIssue = () => router.push("/issues/new");
 
-  if (isLoading) {
+  const { issues, issuesIsLoading, issuesError } = issueQuery();
+  if (issuesIsLoading) {
     return <Loading />;
   }
-  if (error) {
+  if (issuesError) {
     return <Error />;
   }
 
--- utils/client/index.ts
+++ utils/client/index.ts
@@ -1,6 +1,9 @@
-import { Issue } from "@/types";
+import { Issue } from "@prisma/client";
 import axios from "axios";
+import { QueryClient, useQuery } from "react-query";
+import { isQueryKey } from "react-query/types/core/utils";
 
+// axios client CRUD
 const baseUrl = "/api/issues";
 export const getAllIssues = () => axios.get(`${baseUrl}/all`);
 export const getIssueById = (id: number) => axios.get(`${baseUrl}/one/${id}`);
@@ -10,3 +13,16 @@ export const updateIssue = (id: number, issue: Issue) =>
 export const deleteIssue = (id: number) =>
   axios.delete(`${baseUrl}/delete/${id}`);
 
+// reactQuery
+
+export function issueQuery() {
+  const queryClient = new QueryClient();
+  const {
+    data,
+    isLoading: issuesIsLoading,
+    error: issuesError,
+  } = useQuery({ queryKey: "issues", queryFn: getAllIssues });
+  const issues = data?.data as Issue[];
+
+  return { issues, issuesIsLoading, issuesError };
+}
