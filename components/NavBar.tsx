cd636da146f9642ae88c5c82424c671da2139b15
Daly <dalyna64@gmail.com>
2024-02-24 16:08:05 +0100

    fixx

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -4,22 +4,8 @@ import { usePathname, useRouter } from "next/navigation";
 import { Button } from "primereact/button";
 import { Toolbar } from "primereact/toolbar";
 import { FaBug } from "react-icons/fa";
-import { PrimeReactContext } from "primereact/api";
-import { useContext } from "react";
+
 const NavBar = () => {
-  let primeContext = useContext(PrimeReactContext);
-
-  const handleClick = (theme: string) => {
-    const oldTheme = theme === "dark" ? "light" : "dark";
-    primeContext &&
-      primeContext.changeTheme &&
-      primeContext.changeTheme(
-        `/themes/lara-${oldTheme}-blue/theme.css`,
-        `/themes/lara-${theme}-blue/theme.css`,
-        "theme-link"
-      );
-  };
-
   const currentPath = usePathname();
 
   const links = [
@@ -27,18 +13,30 @@ const NavBar = () => {
     { label: "Issues", href: "/issues" },
   ];
   const router = useRouter();
-  const linkClassName = (link: any) =>
-    classNames({
+
+  const linkClassName = (link: any) => {
+    console.log(currentPath);
+
+    if (
+      currentPath === link.href ||
+      (currentPath === "/issues/new" && link.href === "/issues")
+    ) {
+      return "link-active";
+    } else {
+      return "link-not-active";
+    }
+
+    /*     return classNames({
       "link-active": link.href === currentPath,
-      link: link.href !== currentPath,
-    });
+      "link-not-active": link.href !== currentPath,
+    }); */
+  };
 
   return (
     <Toolbar
       className="toolbar"
       start={
         <>
-          <Button label="dd" onClick={() => handleClick("dark")} />
           <Button text onClick={() => router.push("/")}>
             <FaBug size="1.5rem" />
           </Button>
